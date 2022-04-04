dcb80b60759f5ecb5ccfab26794d0a7a7a7d76e5
youngjuee <juju0555@naver.com>
2022-04-04 21:31:05 +0900

    modify

--- src/JavaCote1.java
+++ src/JavaCote1.java
@@ -1,4 +1,3 @@
-package src;
 
 
 public class JavaCote1 {
@@ -7,8 +6,6 @@ public class JavaCote1 {
         int answer = 0;
         str = str.toUpperCase();
         ch = Character.toUpperCase(ch);
-        System.out.println(str + ch);
-
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) == ch){
                 answer++;
@@ -34,6 +31,6 @@ public class JavaCote1 {
     public static void main(String[] args) {
         JavaCote1 javaCote1 = new JavaCote1();
         char c = 'c';
-        System.out.println(javaCote1.solution2("Computercooler",c));
+        System.out.println(javaCote1.solution("Computercooler",c));
     }
 }
