f1efc491ba86d5507410f1ca4601114e43f97861
youngjuee <juju0555@naver.com>
2022-04-05 21:00:20 +0900

    대소문자변환

--- src/JavaCote2.java
+++ src/JavaCote2.java
@@ -11,12 +11,27 @@ public class JavaCote2 {
         return answer;
     }
 
+    public String solution2(String str){
+        String answer = "";
+        for (char x: str.toCharArray()) {
+            // 대문자면 소문자를 나타내고
+            if(x >= 65 && x <= 90){
+                answer += Character.toLowerCase(x);
+            }else{
+                // x >= 97 && x <= 122
+                // 소문자면 대문자로 나타내라
+                answer += Character.toUpperCase(x);
+            }
+        }
+        return answer;
+    }
+
 
     public static void main(String[] args) {
         String str = "StuDY";
 
         JavaCote2 cote2 = new JavaCote2();
-        System.out.println(cote2.solution(str));
+        System.out.println(cote2.solution2(str));
 
     }
 }
