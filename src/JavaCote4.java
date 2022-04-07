e0dd68d5c765603c88078375885e69bd4e60f2cf
youngjuee <juju0555@naver.com>
2022-04-07 21:20:28 +0900

    4.단어뒤집기

--- src/JavaCote4.java
+++ src/JavaCote4.java
@@ -12,11 +12,30 @@ public class JavaCote4 {
         return answer;
     }
 
+    public ArrayList<String> solution2(String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x: str) {
+            char[] ch = x.toCharArray();
+            int lt = 0;
+            int rt = ch.length-1;
+            while(lt < rt){
+                char tmp = ch[lt];
+                ch[lt] = ch[rt];
+                ch[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String a = String.valueOf(ch);
+            answer.add(a);
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         String[] str = {"good","Time","Big"};
 
         JavaCote4 cote4 = new JavaCote4();
-        for (String x : cote4.solution(str)) {
+        for (String x : cote4.solution2(str)) {
             System.out.println(x);
         }
     }
