9c64852c7ff3e06960d00fb7cb82af559ef20ae1
youngjuee <juju0555@naver.com>
2022-08-02 22:12:52 +0900

    완전탐색

--- src/programmers_solution.java
+++ src/programmers_solution.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class programmers_solution {
 
@@ -34,11 +36,44 @@ public class programmers_solution {
         return answer;
     }
 
+//  코딩테스트 연습
+//  완전탐색
+//  모의고사
+    public static int[] solution2(int[] answers) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] counta = {0,0,0};
+        List<Integer> answer = new ArrayList<Integer>();
+
+        for(int i =0; i<answers.length; i++) {
+            if(answers[i % answers.length] == a[i % a.length]) {
+                counta[0]++;
+            }
+            if(answers[i % answers.length] == b[i % b.length]) {
+                counta[1]++;
+            }
+            if(answers[i % answers.length] == c[i % c.length]) {
+                counta[2]++;
+            }
+        }
+
+        int max = Math.max(counta[0], Math.max(counta[1], counta[2]));
+        int j = 0;
+        for(int k = 0; k < counta.length; k++) {
+
+            if(max == counta[k]) {
+                answer.add(k+1);
+                j++;
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
 
     public static void main(String[] args) {
         for(int j : solution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"}, 2)) {
-
             System.out.println(j);
         }
+
     }
 }
