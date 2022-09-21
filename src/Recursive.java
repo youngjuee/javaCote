1762cc954dd5b20f3b336179a9893656633ad76a
youngjuee <juju0555@naver.com>
2022-09-21 20:21:28 +0900

    피보나치 메모이제이션

--- src/Recursive.java
+++ src/Recursive.java
@@ -18,9 +18,24 @@ public class Recursive {
         }
     }
 
+
+    static int[] fibo;
+    public int fibonacci(int n){
+        if(n == 1) return fibo[1] = 1;
+        if(n == 2) return fibo[2] = 1;
+        else return fibo[n] = fibonacci(n-2) + fibonacci(n-1);
+
+
+    }
+
+
+
     public static void main(String[] args) {
         Recursive T = new Recursive();
         //T.DFS(11);
-        System.out.println(T.factorial(5));
+        //System.out.println(T.factorial(5));
+        fibo = new int[10+1];
+        T.fibonacci(10);
+        System.out.println(fibo[10]);
     }
 }
