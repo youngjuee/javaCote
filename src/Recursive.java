e167fa1181ecde46b53aeb05a0723a1dfde28294
youngjuee <juju0555@naver.com>
2022-09-21 19:52:56 +0900

    factorial

--- src/Recursive.java
+++ src/Recursive.java
@@ -10,8 +10,17 @@ public class Recursive {
 
     }
 
+    public int factorial(int n){
+        if(n == 1){
+            return 1;
+        }else{
+            return n * factorial(n-1);
+        }
+    }
+
     public static void main(String[] args) {
         Recursive T = new Recursive();
-        T.DFS(11);
+        //T.DFS(11);
+        System.out.println(T.factorial(5));
     }
 }
